25ff896abf23d9bc464cc1c7d8f342223afda737
Onni Nikku <onni.nikku@gmail.com>
2022-09-16 09:43:32 +0300

    studentcontroller made

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace university.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentController : ControllerBase
+{
+    [HttpGet("GetAllStudents")]
+    public string GetAllStudents() {
+        return "This will return all students.";
+    }
+
+    [HttpGet("{id}")]
+    public string GetOneStudent(int id) {
+        return "This will return a student which id=" + id;
+    }
+
+    [HttpPost()]
+    public string AddStudent() {
+        return "This will add a new student.";
+    }
+
+    [HttpPut("{id}")]
+    public string UpdateStudent(int id) {
+        return "This will update a student which id=" + id;
+    }
+
+    [HttpDelete("{id}")]
+    public string DeleteStudent(int id) {
+        return "This will delete a student which id=" + id;
+    }
+}
